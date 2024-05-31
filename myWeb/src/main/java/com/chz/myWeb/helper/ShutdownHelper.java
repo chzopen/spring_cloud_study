1e5e6c19c24aa68bd455c2b7d407e36008a14a75
chzopen <chzopen@163.com>
2024-05-31 21:58:47 +0800

    ddd

--- myWeb/src/main/java/com/chz/myWeb/helper/ShutdownHelper.java
+++ myWeb/src/main/java/com/chz/myWeb/helper/ShutdownHelper.java
@@ -1,7 +1,6 @@
 package com.chz.myWeb.helper;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class ShutdownHelper implements ApplicationContextAware {
 
-    @Autowired
     private static ApplicationContext applicationContext;
 
     @Override
