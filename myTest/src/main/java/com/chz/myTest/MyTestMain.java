229d6c07f3d219c355da31aa078de4e4931f0124
chzopen <chzopen@163.com>
2024-06-04 11:38:37 +0800

    ddd

--- myTest/src/main/java/com/chz/myTest/MyTestMain.java
+++ myTest/src/main/java/com/chz/myTest/MyTestMain.java
@@ -1,23 +1,8 @@
 package com.chz.myTest;
 
-import java.util.TreeSet;
-import java.util.concurrent.ConcurrentHashMap;
-
 public class MyTestMain {
 
     public static void main(String[] args) {
-        TreeSet<Integer> treeSet = new TreeSet<>();
-        System.out.println(treeSet.last());
-        treeSet.add(2);
-        treeSet.add(3);
-        treeSet.add(1);
-        treeSet.add(4);
-        treeSet.add(10);
-        System.out.println(treeSet);
-        treeSet.pollFirst();
-        System.out.println(treeSet);
-        treeSet.add(7);
-        System.out.println(treeSet);
-        System.out.println(treeSet.last());
+
     }
 }
