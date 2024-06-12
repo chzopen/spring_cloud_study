5066c2ebc064bc9fbaf791525882c235653277af
chzopen <chzopen@163.com>
2024-06-12 17:43:29 +0800

    ddd

--- myEurekaClient1/src/main/java/com/chz/eurekaClient1/controller/TestController.java
+++ myEurekaClient1/src/main/java/com/chz/eurekaClient1/controller/TestController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Slf4j
 @RestController
 @RequestMapping("/test")
@@ -16,6 +18,16 @@ public class TestController {
 
     @GetMapping("/hello")
     public String hello() {
+        log.info("chz >>> TestController.hello()");
+        try {
+            if( Math.abs(ThreadLocalRandom.current().nextInt() % 2)==0 ) {
+                log.info("chz >>> TestController.hello(), sleep");
+                Thread.sleep(3000L);
+            }
+        } catch (InterruptedException e) {
+            log.info("interrupt", e);
+        }
+        log.info("chz >>> TestController.hello(), result");
         return "Hello: " + serverPort;
     }
 
@@ -31,6 +43,16 @@ public class TestController {
         } catch (InterruptedException e) {
         }
         return "Hello3, Spring Boot Web!";
+    }
+
+    public static void main(String[] args) {
+        int i = ThreadLocalRandom.current().nextInt();
+        System.out.println(i);
+        System.out.println(-2 % 2);
+        System.out.println(3 % 2 == 1);
+        System.out.println(2 % 2 == 0);
+        System.out.println(1 % 2 == 1);
+        System.out.println(0 % 2 == 0);
     }
 
 }
