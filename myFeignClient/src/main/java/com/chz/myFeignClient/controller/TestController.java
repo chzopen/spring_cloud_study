54625c06d88866baa784ee1181ac1acd1ddb247f
chzopen <chzopen@163.com>
2024-06-13 09:43:48 +0800

    ddd

--- myFeignClient/src/main/java/com/chz/myFeignClient/controller/TestController.java
+++ myFeignClient/src/main/java/com/chz/myFeignClient/controller/TestController.java
@@ -1,24 +1,24 @@
-package com.chz.myFeignClient.controller;
-
-import com.chz.myFeignClient.feign.TestBaiduFeignClient;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@Slf4j
-@RestController
-@RequestMapping("/test")
-public class TestController {
-
-    @Autowired
-    private TestBaiduFeignClient testBaiduFeignClient;
-
-    @GetMapping("/test")
-    public String test() {
-        log.info("TestController::test: tid:{}", Thread.currentThread().getId());
-        return testBaiduFeignClient.testGet();
-    }
-
-}
+//package com.chz.myFeignClient.controller;
+//
+//import com.chz.myFeignClient.feign.TestBaiduFeignClient;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@Slf4j
+//@RestController
+//@RequestMapping("/test")
+//public class TestController {
+//
+//    @Autowired
+//    private TestBaiduFeignClient testBaiduFeignClient;
+//
+//    @GetMapping("/test")
+//    public String test() {
+//        log.info("TestController::test: tid:{}", Thread.currentThread().getId());
+//        return testBaiduFeignClient.testGet();
+//    }
+//
+//}
--- myFeignClient/src/main/java/com/chz/myFeignClient/feign/TestBaiduFeignClient.java
+++ myFeignClient/src/main/java/com/chz/myFeignClient/feign/TestBaiduFeignClient.java
@@ -1,16 +1,16 @@
-package com.chz.myFeignClient.feign;
-
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@FeignClient(
-        name = "testBaiduFeignClient",
-        contextId = "com.chz.myFeignClient.feign.TestBaiduFeignClient",
-        url = "http://www.baidu.com"
-)
-public interface TestBaiduFeignClient
-{
-    @GetMapping(value = "/")
-    String testGet();
-}
+//package com.chz.myFeignClient.feign;
+//
+//
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@FeignClient(
+//        name = "testBaiduFeignClient",
+//        contextId = "com.chz.myFeignClient.feign.TestBaiduFeignClient",
+//        url = "http://www.baidu.com"
+//)
+//public interface TestBaiduFeignClient
+//{
+//    @GetMapping(value = "/")
+//    String testGet();
+//}
