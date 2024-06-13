be7837721220a6be1bdb04bfbcc828f440ff87af
chzopen <chzopen@163.com>
2024-06-13 19:03:41 +0800

    ddd

--- myBeanBranch/src/main/java/com/chz/myBeanBranch/bean/BranchBean.java
+++ myBeanBranch/src/main/java/com/chz/myBeanBranch/bean/BranchBean.java
@@ -4,12 +4,20 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.annotation.PostConstruct;
+
 @Slf4j
 @Getter
 @Setter
 // 被【ImportSelector】指过来的类不需要添加【@Component】
 public class BranchBean {
 
-    private String name = "BranchBeanName";
+    private String name;
+
+    @PostConstruct
+    public void init()
+    {
+        name = "BranchBeanName put in from @PostConstruct";
+    }
 
 }
--- myBeanBranch/src/main/java/com/chz/myBeanBranch/importSelector/BranchImportSelector.java
+++ myBeanBranch/src/main/java/com/chz/myBeanBranch/importSelector/BranchImportSelector.java
@@ -10,6 +10,8 @@ public class BranchImportSelector implements ImportSelector
     @Override
     public String[] selectImports(AnnotationMetadata annotationMetadata)
     {
+        // 这里想让哪个类变成bean就在返回值里面返回，所以ImportSelector可以用来做为动态添加bean的一个方法，
+        // 比如扫描某一个自定义注解，然后返回类名，变成bean
         return new String[]{
                 BranchImportConfiguration.class.getName(),
                 BranchController.class.getName(),
