1aae28223263b1cddf6b6a1093b49faa83290378
chzopen <chzopen@163.com>
2024-06-16 10:32:23 +0800

    ddd

--- myNetty/src/main/java/com/chz/myNetty/demo2/server/MyServerChannelInBoundHandler1.java
+++ myNetty/src/main/java/com/chz/myNetty/demo2/server/MyServerChannelInBoundHandler1.java
@@ -4,6 +4,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.ReferenceCountUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.StandardCharsets;
@@ -22,31 +23,37 @@ public class MyServerChannelInBoundHandler1 extends SimpleChannelInboundHandler<
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) {
-        CharSequence charSequence1 = msg.readCharSequence(msg.readableBytes(), StandardCharsets.UTF_8);
-        log.info("MyServerChannelInBoundHandler1.channelRead0[" + ctx.channel().remoteAddress() + "]:" + charSequence1);
-        ByteBuf wbuf = Unpooled.copiedBuffer(charSequence1, StandardCharsets.UTF_8);
+        try {
+            CharSequence charSequence1 = msg.readCharSequence(msg.readableBytes(), StandardCharsets.UTF_8);
+            log.info("MyServerChannelInBoundHandler1.channelRead0[" + ctx.channel().remoteAddress() + "]:" + charSequence1);
 
-        AtomicLong tid1 = new AtomicLong(Thread.currentThread().getId());
-        AtomicLong tid2 = new AtomicLong();
+            AtomicLong tid1 = new AtomicLong(Thread.currentThread().getId());
+            AtomicLong tid2 = new AtomicLong();
 
-        ctx.channel().eventLoop().execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    tid2.set(Thread.currentThread().getId());
-                    if( Objects.equals(tid1.get(), tid2.get()) ){
-                        log.info("equals(tid1, tid2)");
-                    } else {
-                        log.info("not equals(tid1, tid2)");
+            ctx.channel().eventLoop().execute(new Runnable() {
+                @Override
+                public void run() {
+                    ByteBuf wbuf = Unpooled.copiedBuffer(charSequence1, StandardCharsets.UTF_8);
+                    try {
+                        tid2.set(Thread.currentThread().getId());
+                        if( Objects.equals(tid1.get(), tid2.get()) ){
+                            log.info("equals(tid1, tid2)");
+                        } else {
+                            log.info("not equals(tid1, tid2)");
+                        }
+                        log.info("channelRead0, runnable 1: start, tid2={}", tid2.get());
+                        Thread.sleep(1000L);
+                        ctx.writeAndFlush(wbuf);
+                        log.info("channelRead0, runnable 1: end, tid2={}", tid2.get());
+                    } catch (InterruptedException e) {
+                    } finally {
+                        ReferenceCountUtil.release(wbuf);
                     }
-                    log.info("channelRead0, runnable 1: start, tid2={}", tid2.get());
-                    Thread.sleep(1000L);
-                    ctx.writeAndFlush(wbuf);
-                    log.info("channelRead0, runnable 1: end, tid2={}", tid2.get());
-                } catch (InterruptedException e) {
                 }
-            }
-        });
+            });
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
 
     @Override
@@ -55,14 +62,4 @@ public class MyServerChannelInBoundHandler1 extends SimpleChannelInboundHandler<
         super.channelInactive(ctx);
     }
 
-    public static void main(String[] args) {
-        ByteBuf heapBuf = Unpooled.buffer(10);
-        if (heapBuf.hasArray()) {
-            byte[] array = heapBuf.array();
-            int offset = heapBuf.arrayOffset() + heapBuf.readerIndex();
-            int length = heapBuf.readableBytes();
-            //0,0
-            log.info("offset:{},length:{}", offset, length);
-        }
-    }
 }
